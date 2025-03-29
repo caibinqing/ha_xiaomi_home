e74ffb5ce50899fb2344c6a020c949dd81e17909
caibinqing <caibinqing@live.com>
2025-03-29 23:12:17 +0800

    spec: 小米门窗传感器2 小米人体传感器2 米家夜灯2 illumination-state

--- custom_components/xiaomi_home/miot/specs/specv2entity.py
+++ custom_components/xiaomi_home/miot/specs/specv2entity.py
@@ -582,7 +582,7 @@ SPEC_PROP_TRANS_MAP: dict = {
             'access': {'read'}
         },
         'binary_sensor': {
-            'format': {'bool', 'int'},
+            'format': {'bool', 'int', 'float'},
             'access': {'read'}
         },
         'switch': {
@@ -605,6 +605,10 @@ SPEC_PROP_TRANS_MAP: dict = {
         },
         'charging-state': {
             'device_class': BinarySensorDeviceClass.BATTERY_CHARGING,
+            'entity': 'binary_sensor',
+        },
+        'illumination-state': {
+            'device_class': BinarySensorDeviceClass.LIGHT,
             'entity': 'binary_sensor',
         },
         'temperature': {
